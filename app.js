741bfc83448e754e6ee0b44664e59f7b31739055
paudefclasspy <77154387+paudefclasspy@users.noreply.github.com>
2023-03-25 13:33:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const form = document.querySelector('form');
 const listaDeseos = document.querySelector('#lista-deseos');
 
+let deseos = JSON.parse(localStorage.getItem('deseos')) || [];
+
+// Función para guardar los deseos en el almacenamiento local
+function guardarDeseos() {
+  localStorage.setItem('deseos', JSON.stringify(deseos));
+}
+
 // Función para eliminar elemento de la lista de deseos
 function eliminarDeseo(elemento) {
   elemento.remove();
+  const index = deseos.indexOf(elemento.innerText.slice(0,-8));
+  deseos.splice(index, 1);
+  guardarDeseos();
 }
 
 // Agregar evento 'click' a la lista de deseos
@@ -18,12 +28,30 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const input = form.elements.deseo;
   const nuevoDeseo = document.createElement('li');
-  nuevoDeseo.innerText = input.value;
+  const textoDeseo = document.createTextNode(input.value);
+  nuevoDeseo.appendChild(textoDeseo);
   
   const botonEliminar = document.createElement('button');
   botonEliminar.innerText = 'Eliminar';
   nuevoDeseo.appendChild(botonEliminar);
   
   listaDeseos.appendChild(nuevoDeseo);
+  deseos.push(input.value);
+  guardarDeseos();
   input.value = '';
 });
+
+// Recuperar los deseos del almacenamiento local al cargar la página
+window.onload = () => {
+  deseos.forEach((deseo) => {
+    const nuevoDeseo = document.createElement('li');
+    const textoDeseo = document.createTextNode(deseo);
+    nuevoDeseo.appendChild(textoDeseo);
+  
+    const botonEliminar = document.createElement('button');
+    botonEliminar.innerText = 'Eliminar';
+    nuevoDeseo.appendChild(botonEliminar);
+  
+    listaDeseos.appendChild(nuevoDeseo);
+  });
+};
